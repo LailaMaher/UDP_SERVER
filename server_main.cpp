02f35ba90b07d9e88f9ebf203bfbdc37c2b17417
laurent <laurent.thomas@open-cells.com>
2018-01-11 08:35:52 +0200

    initial

---
+++ server_main.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include "Server.h"
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "Creating Server"<<endl;
+
+	Server* tcp = Server::instance();
+	tcp->createUDPSocket(atoi(argv[1]));
+
+
+	while(true){
+		cout << "Accepting user\n";
+		tcp->acceptUser();
+	}
+
+	
+	return 0;
+}
